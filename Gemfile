ee00861e1053d69730c837d231ad5bfb59ae3e0c
Samuel Cochran <sj26@sj26.com>
2015-12-12 15:21:45 +1100

    Use dotenv

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "rails", "4.1.1"
+gem "dotenv"
 
 # Database
 gem "pg"
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,19 @@
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+
+require "dotenv"
+
+dotenv_root = File.expand_path("../../", __FILE__)
+dotenv_env = ENV.fetch("RAILS_ENV", ENV.fetch("RACK_ENV", "development"))
+dotenv_files = ["#{dotenv_root}/.env.#{dotenv_env}", "#{dotenv_root}/.env"]
+
+if ENV.include? "DOTENV_LOADED"
+  ENV["DOTENV_LOADED"].split(":").each do |key|
+    ENV.delete(key)
+  end
+end
+
+dotenv_loaded = Dotenv.load(*dotenv_files)
+
+ENV["DOTENV_LOADED"] = dotenv_loaded.keys.join(":")
